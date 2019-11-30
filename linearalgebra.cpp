0715a5d7632a875e4c3118cf969fc670ec561fe2
Anatolii Shablov <anatoliishablov@gmail.com>
2019-11-30 23:17:12 +0300

    transpose bug fixed, post-*crement changed to pre-*crement

--- linearalgebra.cpp
+++ linearalgebra.cpp
@@ -89,10 +89,10 @@ namespace la {
             throw std::runtime_error(error);
         }
         matrix tmp(amount_of_rows_, rhs.amount_of_columns_);
-        for (size_t i = 0; i < amount_of_rows_; i++) {
-            for (size_t j = 0; j < rhs.amount_of_columns_; j++) {
+        for (size_t i = 0; i < amount_of_rows_; ++i) {
+            for (size_t j = 0; j < rhs.amount_of_columns_; ++j) {
                 tmp(i, j) = operator()(i, 0) * rhs(0, j);
-                for (size_t k = 1; k < amount_of_columns_; k++) {
+                for (size_t k = 1; k < amount_of_columns_; ++k) {
                     tmp(i, j) += operator()(i, k) * rhs(k, j);
                 }
             }
@@ -128,9 +128,9 @@ namespace la {
 
         std::vector<double> tmp(data_.begin(), data_.end());
         double res = 1.0;
-        for (size_t i = 0; i < amount_of_rows_; i++) {
+        for (size_t i = 0; i < amount_of_rows_; ++i) {
             size_t change = i;
-            for (; change < amount_of_rows_; change++) {
+            for (; change < amount_of_rows_; ++change) {
                 if (tmp[change * amount_of_columns_ + i] != 0) break;
             }
             if (change == amount_of_rows_) {
@@ -143,7 +143,7 @@ namespace la {
                                  tmp.begin() + (i + 1) * amount_of_columns_,
                                  tmp.begin() + change * amount_of_columns_);
             }
-            for (size_t k = i + 1; k < amount_of_rows_; k++) {
+            for (size_t k = i + 1; k < amount_of_rows_; ++k) {
                 double mn = -1 * (tmp[k * amount_of_columns_ + i] / tmp[i * amount_of_columns_ + i]);
                 std::transform(tmp.begin() + i * amount_of_columns_,
                                tmp.begin() + (i + 1) * amount_of_columns_,
@@ -153,7 +153,7 @@ namespace la {
             }
         }
 
-        for (size_t i = 0; i < amount_of_rows_; i++)
+        for (size_t i = 0; i < amount_of_rows_; ++i)
             res *= tmp[i * amount_of_columns_ + i];
         return res;
     }
@@ -164,8 +164,8 @@ namespace la {
             return *this;
         }
         if (amount_of_rows_ == amount_of_columns_) {
-            for (size_t i = 0; i < amount_of_rows_; i++) {
-                for (size_t j = 0; j < amount_of_columns_; j++) {
+            for (size_t i = 0; i < amount_of_rows_ / 2; ++i) {
+                for (size_t j = 0; j < amount_of_columns_ / 2; ++j) {
                     std::swap(data_[i * amount_of_columns_ + j], data_[i + j * amount_of_rows_]);
                 }
             }
@@ -175,7 +175,7 @@ namespace la {
         size_t i = 0;
         while (i < data_.size()) {
             if (state[i]) {
-                i++;
+                ++i;
                 continue;
             }
             state[i] = true;
@@ -204,18 +204,18 @@ namespace la {
 
         std::vector<double> tmp[2] = {std::vector<double>(amount_of_rows_ * amount_of_rows_),
                                       std::vector<double>(amount_of_rows_ * amount_of_rows_)};
-        for (size_t i = 0; i < data_.size(); i++) {
+        for (size_t i = 0; i < data_.size(); ++i) {
             tmp[0][i] = data_[i];
         }
-        for (size_t i = 0; i < amount_of_rows_; i++) {
-            for (size_t j = 0; j < amount_of_columns_; j++) {
+        for (size_t i = 0; i < amount_of_rows_; ++i) {
+            for (size_t j = 0; j < amount_of_columns_; ++j) {
                 tmp[1][i * amount_of_columns_ + j] = (i == j) ? 1.0 : 0.0;
             }
         }
 
-        for (size_t i = 0; i < amount_of_rows_; i++) {
+        for (size_t i = 0; i < amount_of_rows_; ++i) {
             size_t change = i;
-            for (; change < amount_of_rows_; change++) {
+            for (; change < amount_of_rows_; ++change) {
                 if (tmp[0][change * amount_of_columns_ + i] != 0) break;
             }
             if (change == amount_of_rows_) {
@@ -236,7 +236,7 @@ namespace la {
                               [mn](auto &lhs) { lhs *= mn; });
             }
 
-            for (size_t k = i + 1; k < amount_of_rows_; k++) {
+            for (size_t k = i + 1; k < amount_of_rows_; ++k) {
                 mn = -1.0 * tmp[0][k * amount_of_columns_ + i];
                 for (auto &t : tmp) {
                     std::transform(t.begin() + i * amount_of_columns_,
@@ -247,8 +247,8 @@ namespace la {
                 }
             }
         }
-        for (size_t i = amount_of_rows_; i--;) {
-            for (size_t j = i + 1; j < amount_of_rows_; j++) {
+        for (size_t i = amount_of_rows_; --i;) {
+            for (size_t j = i + 1; j < amount_of_rows_; ++j) {
                 double mn = -1.0 * tmp[0][i * amount_of_columns_ + j];
                 std::transform(tmp[1].begin() + i * amount_of_columns_,
                                tmp[1].begin() + (i + 1) * amount_of_columns_,
@@ -270,8 +270,8 @@ namespace la {
     }
 
     std::ostream &operator<<(std::ostream &cout, matrix const &a) {
-        for (size_t i = 0; i < a.amount_of_columns_; i++) {
-            for (size_t j = 0; j < a.amount_of_rows_; j++) {
+        for (size_t i = 0; i < a.amount_of_columns_; ++i) {
+            for (size_t j = 0; j < a.amount_of_rows_; ++j) {
                 cout << a(i, j) << ' ';
             }
             cout << '\n';
@@ -283,8 +283,8 @@ namespace la {
         size_t h, l;
         cin >> h >> l;
         a = matrix(h, l);
-        for (size_t i = 0; i < h; i++) {
-            for (size_t j = 0; j < l; j++) {
+        for (size_t i = 0; i < h; ++i) {
+            for (size_t j = 0; j < l; ++j) {
                 cin >> a(i, j);
             }
         }
@@ -301,8 +301,8 @@ namespace la {
 
     matrix E(size_t n) {
         matrix result(n, n);
-        for (size_t i = 0; i < n; i++) {
-            for (size_t j = 0; j < n; j++) {
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < n; ++j) {
                 result(i, j) = (i == j) ? 1.0 : 0.0;
             }
         }
@@ -325,9 +325,9 @@ namespace la {
 
     double tensor::sum_el(std::vector<size_t> const &old, std::vector<size_t> const &real, matrix T_matrix, matrix S_matrix) {
         double res = operator()(old);
-        for (size_t i = 0; i < amount_of_p_; i++)
+        for (size_t i = 0; i < amount_of_p_; ++i)
             res *= S_matrix(real[i], old[i]);
-        for (size_t i = 0; i < amount_of_q_; i++)
+        for (size_t i = 0; i < amount_of_q_; ++i)
             res *= T_matrix(old[i + amount_of_p_], real[i + amount_of_p_]);
         return res;
     }
@@ -337,11 +337,11 @@ namespace la {
         size_t real_i = (round.size() > 1) ? 1 : 0;
         while ((i != round.size()) && (round[real_i] + 1 == dimension_)) {
             round[real_i] = 0;
-            i++;
+            ++i;
             real_i = (round.size() % 2 == 0 || round.size() - 1 != i) ? i ^ 1 : i;
         }
         if (i < round.size()) {
-            round[real_i]++;
+            ++round[real_i];
             return true;
         }
         return false;
@@ -350,14 +350,14 @@ namespace la {
     bool next_permutation_mn(std::vector<size_t> &permutation, double &mn) {
         size_t i = permutation.size() - 1;
         while (i > 0 && permutation[i] < permutation[i - 1]) {
-            i--;
+            --i;
         }
 
         if (i == 0) {
             return false;
         }
         size_t pos = i;
-        for (size_t j = i + 1; j < permutation.size(); j++) {
+        for (size_t j = i + 1; j < permutation.size(); ++j) {
             if (permutation[j] > permutation[i - 1] && permutation[j] < permutation[pos]) {
                 pos = j;
             }
@@ -368,7 +368,7 @@ namespace la {
         bool is_sorted = false;
         while (!is_sorted) {
             is_sorted = true;
-            for (size_t j = i + 1; j < permutation.size(); j++) {
+            for (size_t j = i + 1; j < permutation.size(); ++j) {
                 if (permutation[j] < permutation[j - 1]) {
                     is_sorted = false;
                     mn *= -1.0;
@@ -432,14 +432,14 @@ namespace la {
         std::vector<size_t> round(amount_of_p_ + amount_of_q_ - 2, 0);
         std::vector<size_t> round_real(amount_of_p_ + amount_of_q_);
         do {
-            for (size_t i = 0, j = 0; i < round_real.size(); i++) {
+            for (size_t i = 0, j = 0; i < round_real.size(); ++i) {
                 if (i == p_num || i == amount_of_p_ + q_num) continue;
                 round_real[i] = round[j++];
             }
             round_real[p_num] = 0;
             round_real[amount_of_p_ + q_num] = 0;
             result(round) = operator()(round_real);
-            for (size_t i = 1; i < dimension_; i++) {
+            for (size_t i = 1; i < dimension_; ++i) {
                 round_real[p_num] = i;
                 round_real[amount_of_p_ + q_num] = i;
                 result(round) += operator()(round_real);
@@ -483,8 +483,10 @@ namespace la {
             throw std::runtime_error("Can't get element from bad-size vector");
         }
         size_t cnt = 0;
-        for (size_t i = 0; i < v.size(); i++) {
-            if (v[i]) cnt++;
+        for (size_t i = 0; i < v.size(); ++i) {
+            if (v[i]) {
+	    	++cnt;
+	    }
         }
         if (cnt < 2) {
             return *this;
@@ -504,28 +506,28 @@ namespace la {
             double sum = 0;
             size_t amount = 0;
             std::vector<size_t> permutation(cnt);
-            for (size_t i(0), j(0); i < v.size(); i++) {
+            for (size_t i(0), j(0); i < v.size(); ++i) {
                 if (!v[i]) continue;
                 permutation[j++] = i;
             }
             do {
-                for (size_t i(0), j(0); i < round.size(); i++) {
+                for (size_t i(0), j(0); i < round.size(); ++i) {
                     if (v[i]) {
                         round[i] = state_round[permutation[j++]];
                     } else {
                         round[i] = state_round[i];
                     }
                 }
-                amount++;
+                ++amount;
                 sum += operator()(round);
             } while (std::next_permutation(permutation.begin(), permutation.end()));
             sum /= amount;
-            for (size_t i(0), j(0); i < v.size(); i++) {
+            for (size_t i(0), j(0); i < v.size(); ++i) {
                 if (!v[i]) continue;
                 permutation[j++] = i;
             }
             do {
-                for (size_t i(0), j(0); i < round.size(); i++) {
+                for (size_t i(0), j(0); i < round.size(); ++i) {
                     if (v[i]) {
                         round[i] = state_round[permutation[j++]];
                     } else {
@@ -548,8 +550,10 @@ namespace la {
             throw std::runtime_error("Can't get element from bad-size vector");
         }
         size_t cnt = 0;
-        for (size_t i = 0; i < v.size(); i++) {
-            if (v[i]) cnt++;
+        for (size_t i = 0; i < v.size(); ++i) {
+            if (v[i]) {
+	    	++cnt;
+	    }
         }
         if (cnt < 2) {
             return *this;
@@ -570,29 +574,29 @@ namespace la {
             double sum = 0;
             size_t amount = 0;
             std::vector<size_t> permutation(cnt);
-            for (size_t i(0), j(0); i < v.size(); i++) {
+            for (size_t i(0), j(0); i < v.size(); ++i) {
                 if (!v[i]) continue;
                 permutation[j++] = i;
             }
             do {
-                for (size_t i(0), j(0); i < round.size(); i++) {
+                for (size_t i(0), j(0); i < round.size(); ++i) {
                     if (v[i]) {
                         round[i] = state_round[permutation[j++]];
                     } else {
                         round[i] = state_round[i];
                     }
                 }
-                amount++;
+                ++amount;
                 sum += mn * operator()(round);
             } while (next_permutation_mn(permutation, mn));
             sum /= amount;
-            for (size_t i(0), j(0); i < v.size(); i++) {
+            for (size_t i(0), j(0); i < v.size(); ++i) {
                 if (!v[i]) continue;
                 permutation[j++] = i;
             }
             mn = 1.0;
             do {
-                for (size_t i(0), j(0); i < round.size(); i++) {
+                for (size_t i(0), j(0); i < round.size(); ++i) {
                     if (v[i]) {
                         round[i] = state_round[permutation[j++]];
                     } else {
@@ -618,7 +622,7 @@ namespace la {
         do {
 
             size_t max = 0;
-            for (size_t i = 0; i < v.size(); i++) {
+            for (size_t i = 0; i < v.size(); ++i) {
                 if (v[i] != prev[i]) {
                     max = std::max(max, (v.size() % 2 == 0 || v.size() - 1 != i) ? i ^ 1 : i);
                 }
@@ -631,7 +635,7 @@ namespace la {
                 cout << '\n';
             } else {
                 cout << '\n';
-                for (size_t j = 0; j < max - 1; j++)
+                for (size_t j = 0; j < max - 1; ++j)
                     cout << "_____________\n";
             }
             cout << a(v);
